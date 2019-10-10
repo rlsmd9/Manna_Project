9dc93642495de66fc7dd024cd3097d7eff1e6c65
JungJiSeung <rlsmd9@naver.com>
2019-10-10 22:31:25 +0900

    메인 약속 액티비티에 탭 호스트 추가

--- app/src/main/java/com/example/manna_project/main_agreement.java
+++ app/src/main/java/com/example/manna_project/main_agreement.java
@@ -3,6 +3,7 @@ package com.example.manna_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class main_agreement extends AppCompatActivity {
 
@@ -10,5 +11,28 @@ public class main_agreement extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_agreement);
+
+        TabHost tabHost = findViewById(R.id.host);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("tab01_friend");
+        tabSpec.setIndicator("친구");
+        tabSpec.setContent(R.id.tab01_friend);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tab02_friend");
+        tabSpec.setIndicator("약속");
+        tabSpec.setContent(R.id.tab02_friend);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tab03_friend");
+        tabSpec.setIndicator("일정");
+        tabSpec.setContent(R.id.tab03_friend);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tab04_friend");
+        tabSpec.setIndicator("설정");
+        tabSpec.setContent(R.id.tab04_friend);
+        tabHost.addTab(tabSpec);
     }
 }
