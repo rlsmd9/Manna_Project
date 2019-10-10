c340408a9e976d3f5cced6f8a47c28bd0ea4ca4b
vogjog <hackwaffle@gmail.com>
2019-10-10 19:10:38 +0900

    Friend_List 1st commit

---
+++ app/src/main/java/com/example/manna_project/Friend_List.java
@@ -0,0 +1,14 @@
+package com.example.manna_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Friend_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend__list);
+    }
+}
