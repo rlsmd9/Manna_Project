040596dc0fc66dbd316f3978f0d9faeda959fd7d
JungJiSeung <rlsmd9@naver.com>
2019-10-13 18:57:11 +0900

    firebase server 추가

--- app/src/main/java/com/example/manna_project/Login_activity.java
+++ app/src/main/java/com/example/manna_project/Login_activity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Login_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
