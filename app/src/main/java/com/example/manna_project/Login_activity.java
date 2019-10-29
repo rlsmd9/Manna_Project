8efcd536e95b91b7691d55d8b0ab9465cba1a119
JungJiSeung <rlsmd9@naver.com>
2019-10-29 14:54:06 +0900

    Firebase SHA1 인증 추가

--- app/src/main/java/com/example/manna_project/Login_activity.java
+++ app/src/main/java/com/example/manna_project/Login_activity.java
@@ -5,19 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class Login_activity extends AppCompatActivity {
+public class Login_activity extends AppCompatActivity implements View.OnClickListener {
     final String TAG = "MANNA";
+    private FirebaseAuth mAuth;
+
+    EditText email, pw;
+
+    Button login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+
+        email = findViewById(R.id.login_id);
+        pw = findViewById(R.id.login_pw);
+        login = findViewById(R.id.login_confirm);
+
+        mAuth = FirebaseAuth.getInstance();
 
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
         DatabaseReference myRef = rootRef.getRoot();
@@ -38,6 +55,30 @@ public class Login_activity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == login) {
+
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user == null) {
+
+        } else {
+
+        }
     }
 
     private void checkUpdateVersion(DBData data) {
