73d671f3ac9acb667fb781e7caa7ea9892f44816
JungJiSeung <rlsmd9@naver.com>
2019-10-31 20:09:13 +0900

    Colors 리소스 파일에 lightBlue, lightRed 추가

--- app/src/main/java/com/example/manna_project/main_agreement.java
+++ app/src/main/java/com/example/manna_project/main_agreement.java
@@ -19,13 +19,16 @@ public class main_agreement extends AppCompatActivity {
 
         TabHost.TabSpec tabSpec = tabHost.newTabSpec("tab01_friend");
         tabSpec.setIndicator("친구");
+        tabSpec.setContent(R.id.tab02_friend);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tab_agreement");
+        tabSpec.setIndicator("약속");
         tabSpec.setContent(R.id.agreementCalendar);
         tabHost.addTab(tabSpec);
 
-        tabSpec = tabHost.newTabSpec("tab02_friend");
-        tabSpec.setIndicator("약속");
-        tabSpec.setContent(R.id.tab02_friend);
-        tabHost.addTab(tabSpec);
+        // 달력 생성
+        makeCalendar();
 
         tabSpec = tabHost.newTabSpec("tab03_friend");
         tabSpec.setIndicator("일정");
@@ -36,5 +39,9 @@ public class main_agreement extends AppCompatActivity {
         tabSpec.setIndicator("설정");
         tabSpec.setContent(R.id.tab04_friend);
         tabHost.addTab(tabSpec);
+    }
+
+    private void makeCalendar() {
+
     }
 }
