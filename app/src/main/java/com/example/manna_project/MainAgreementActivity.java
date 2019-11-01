43bd8db12218595ee5b2173f9ea13b58ec8cf3e9
JungJiSeung <rlsmd9@naver.com>
2019-11-01 21:19:56 +0900

    java 소스에서 weight 속성 사용을 위해서 api레벨 21로 업그레이드 MainAgreementActivity 캘린더 날짜 출력 완료

--- app/src/main/java/com/example/manna_project/MainAgreementActivity.java
+++ app/src/main/java/com/example/manna_project/MainAgreementActivity.java
@@ -2,22 +2,18 @@ package com.example.manna_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.GridLayout;
-import android.widget.LinearLayout;
 import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.example.manna_project.MainAgreementActivity_Util.Custom_Calendar;
+
+import java.util.Calendar;
 
 public class MainAgreementActivity extends AppCompatActivity {
 
-    GridLayout calendar_root_grid;
+    Custom_Calendar custom_calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +26,14 @@ public class MainAgreementActivity extends AppCompatActivity {
         TabHost.TabSpec tabSpec = tabHost.newTabSpec("tab01_friend");
         tabSpec.setIndicator("친구");
         tabSpec.setContent(R.id.tab02_friend);
-        calendar_root_grid = findViewById(R.id.dateRootGrid);
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tab_agreement");
         tabSpec.setIndicator("약속");
         tabSpec.setContent(R.id.agreementCalendar);
         tabHost.addTab(tabSpec);
-
-        // 달력 생성
-        makeCalendar();
+        custom_calendar = new Custom_Calendar(this, (GridLayout) findViewById(R.id.main_agreement_calendarGridLayout), Calendar.getInstance());
+        custom_calendar.makeCalendar();
 
         tabSpec = tabHost.newTabSpec("tab03_friend");
         tabSpec.setIndicator("일정");
@@ -50,35 +44,7 @@ public class MainAgreementActivity extends AppCompatActivity {
         tabSpec.setIndicator("설정");
         tabSpec.setContent(R.id.tab04_friend);
         tabHost.addTab(tabSpec);
-
-        Toast.makeText(this, calendar_root_grid.getWidth()+"", Toast.LENGTH_SHORT);
     }
 
-    private void makeCalendar() {
-//        calendar_root_grid.removeAllViews();
 
-        // activity_main_agreement의 약속탭에 달력 만들기
-        for (int i = 1; i <= 7; i++) {
-            for (int j = 1; j <= 6; j++) {
-                TextView tv = new TextView(this);
-                tv.setWidth(100);
-                GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams();
-                layoutParams.width = GridLayout.LayoutParams.WRAP_CONTENT;
-
-//                Log.d("mannad", calendar_root_grid.getWidth()/7+"");
-                layoutParams.height = GridLayout.LayoutParams.WRAP_CONTENT;
-                layoutParams.columnSpec = GridLayout.spec(i-1);
-                layoutParams.rowSpec = GridLayout.spec(j);
-                tv.setText("DW");
-                tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                tv.setLayoutParams(layoutParams);
-//
-//                calendar_root_grid.addView(tv);
-//                LinearLayout ly = new LinearLayout(this);
-//                ly.setOrientation(LinearLayout.VERTICAL);
-//                ly.setBackgroundColor(Color.rgb(255,255,0));
-                calendar_root_grid.addView(tv);
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/example/manna_project/MainAgreementActivity_Util/Custom_Calendar.java
@@ -0,0 +1,89 @@
+package com.example.manna_project.MainAgreementActivity_Util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.manna_project.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class Custom_Calendar {
+    Context context;
+    LayoutInflater layout;
+    Calendar date;
+    GridLayout calendar_root_grid;
+    // 일정 데이터 받아서 저장할 자료구조
+    ArrayList<CustomSchedule> datalist;
+
+    public Custom_Calendar(Context context, GridLayout calendar_root_grid, Calendar date) {
+        this.context = context;
+        this.layout = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.calendar_root_grid = calendar_root_grid;
+        this.date = date;
+    }
+
+    public Custom_Calendar(Context context, GridLayout calendar_root_grid, Calendar date, ArrayList<CustomSchedule> datalist) {
+        this.context = context;
+        this.date = date;
+        this.datalist = datalist;
+        this.calendar_root_grid = calendar_root_grid;
+        this.layout = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void makeCalendar() {
+        // activity_main_agreement의 약속탭에 달력 만들기
+        int cnt = 1;
+        int start, end;
+        Calendar c = Calendar.getInstance();
+        c.set(date.get(Calendar.YEAR), date.get(Calendar.MONTH), 1);
+        start = c.get(Calendar.DAY_OF_WEEK);
+        c.set(date.get(Calendar.YEAR), date.get(Calendar.MONTH)+1, 0);
+        end = c.get(Calendar.DAY_OF_MONTH);
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                LinearLayout ly = (LinearLayout) layout.inflate(R.layout.activity_main_agreement_calendar_item, null);
+                GridLayout.LayoutParams param = new GridLayout.LayoutParams(GridLayout.spec(
+                        GridLayout.UNDEFINED,GridLayout.FILL,1f),
+                        GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f));
+
+                LinearLayout.LayoutParams linearParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+                TextView textView = new TextView(context);
+                textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                textView.setTypeface(null,Typeface.BOLD);
+
+                textView.setEnabled(true);
+                // j+(7*i)+1 = for문 순서 1~42
+                Log.d("manna_js", "makeCalendar: 1");
+                c.set(date.get(Calendar.YEAR), date.get(Calendar.MONTH), (cnt++)-start+1);
+                if (j+(7*i)+1 < date.get(Calendar.DAY_OF_WEEK) || j+(7*i)+1 > (end+start-1)) {
+                    textView.setEnabled(false);
+                } else {
+                    if (c.get(Calendar.DAY_OF_WEEK) == 1) {
+                        textView.setTextColor(context.getResources().getColor(R.color.lightRed));
+                    } else if (c.get(Calendar.DAY_OF_WEEK) == 7) {
+                        textView.setTextColor(context.getResources().getColor(R.color.lightBlue));
+                    }
+                }
+
+
+
+                textView.setText(c.get(Calendar.DAY_OF_MONTH) + "");
+                ly.addView(textView,linearParams);
+                calendar_root_grid.addView(ly,param);
+            }
+        }
+    }
+}
+
+class CustomSchedule {
+
+}
