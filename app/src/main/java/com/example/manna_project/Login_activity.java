8186146e6e337dce9710a6323027e23d2707e890
Yechan Jin <roqhrryghl@gmail.com>
2019-11-07 19:24:56 +0900

    구글로그인 기능 구현 구글 로그인과 firebase 연동 완료

--- app/src/main/java/com/example/manna_project/Login_activity.java
+++ app/src/main/java/com/example/manna_project/Login_activity.java
@@ -3,14 +3,27 @@ package com.example.manna_project;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,52 +31,87 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Login_activity extends AppCompatActivity implements View.OnClickListener {
+
     final String TAG = "MANNA";
+
+    private static final int RC_SIGN_IN = 703;
     private FirebaseAuth mAuth;
 
-    EditText email, pw;
+    private GoogleSignInClient googleSignInClient;
 
-    Button login;
+    Button googleLoginBtn;
+
+    EditText id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
 
-        email = findViewById(R.id.login_id);
-        pw = findViewById(R.id.login_pw);
-        login = findViewById(R.id.login_confirm);
-
         mAuth = FirebaseAuth.getInstance();
+        onStart();
 
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference myRef = rootRef.getRoot();
+        googleLoginBtn = findViewById(R.id.login_confirm);
+        id = findViewById(R.id.login_id);
 
-                myRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        DBData data = dataSnapshot.child("version").getValue(DBData.class);
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
 
-                        checkUpdateVersion(data);
-                    }
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        Log.d(TAG, databaseError.getMessage());
+        googleLoginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent signinIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signinIntent,RC_SIGN_IN);
             }
         });
 
-
-
     }
-
     @Override
-    public void onClick(View v) {
-        if (v == login) {
+    public void onClick(View v){
+        Intent intent = new Intent(this, MainAgreementActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        // 로그인 액티비티가 잘 실행됬는가 판단
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("manna_yc","구글 로그인 성공 " + account.getId());
+                firebaseAuthWithGoogle(account);
+            }
+            catch(ApiException e){
+                e.printStackTrace();
+                Log.d("manna_yc","구글 로그인 실패");
+            }
         }
     }
 
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "로그인 성공");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                               // updateUI(user);
+                            } else {
+                            Log.w(TAG, "로그인 실패", task.getException());
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
 
     @Override
     protected void onStart() {
